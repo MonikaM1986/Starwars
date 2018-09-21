a2edc1603669d4926a1150a33e55c897d05b8152
USER-PC\USER <mmartinez@telenium.es>
2018-09-21 12:30:07 +0200

    Routing Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FindFilmComponent} from './components/find-film/find-film.component';
+import { AppRoutingModule} from './app-routing.module';
+import {routingModule} from './app-routing.module';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindFilmComponent,
+    DetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    routingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
